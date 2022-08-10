742950dc0efda99d8babfbae07c6798e21f93c5b
Bartosz Osiński <kojar1997@gmail.com>
2022-08-10 14:43:19 +0200

    nft 3 d main

--- scripts/deployCollection.js
+++ scripts/deployCollection.js
@@ -8,52 +8,16 @@ async function main() {
 
   const Nft = await hre.ethers.getContractFactory('Nft');
     const uris = [
-    "https://jsonkeeper.com/b/1G7S", // Thunder Dragon
-    "https://jsonkeeper.com/b/1G7S", // Thunder Dragon
-    "https://jsonkeeper.com/b/1G7S", // Thunder Dragon
-    "https://jsonkeeper.com/b/JIWW", // Thunder Dragon Colossus
-    "https://jsonkeeper.com/b/JIWW", // Thunder Dragon Colossus
-    "https://jsonkeeper.com/b/JIWW", // Thunder Dragon Colossus
-    "https://jsonkeeper.com/b/JCT3", // Thunder Dragon Thunderstormech
-    "https://jsonkeeper.com/b/JCT3", // Thunder Dragon Thunderstormech
-    "https://jsonkeeper.com/b/JCT3", // Thunder Dragon Thunderstormech
-    "https://jsonkeeper.com/b/8UBU", // Thunder Dragon Titan
-    "https://jsonkeeper.com/b/8UBU", // Thunder Dragon Titan
-    "https://jsonkeeper.com/b/8UBU", // Thunder Dragon Titan
-    "https://jsonkeeper.com/b/A84J", // Thunder Dragondark
-    "https://jsonkeeper.com/b/A84J", // Thunder Dragondark
-    "https://jsonkeeper.com/b/A84J", // Thunder Dragondark
-    "https://jsonkeeper.com/b/ULJ7", // Thunder Dragonduo
-    "https://jsonkeeper.com/b/ULJ7", // Thunder Dragonduo
-    "https://jsonkeeper.com/b/ULJ7", // Thunder Dragonduo
-    "https://jsonkeeper.com/b/Z0TY", // Thunder Dragonhawk
-    "https://jsonkeeper.com/b/Z0TY", // Thunder Dragonhawk
-    "https://jsonkeeper.com/b/Z0TY", // Thunder Dragonhawk
-    "https://jsonkeeper.com/b/YRNC", // Twin-Headed Thunder Dragon
-    "https://jsonkeeper.com/b/YRNC", // Twin-Headed Thunder Dragon
-    "https://jsonkeeper.com/b/YRNC", // Twin-Headed Thunder Dragon
-    "https://jsonkeeper.com/b/KI8E", // Thunder Dragonroar
-    "https://jsonkeeper.com/b/KI8E", // Thunder Dragonroar
-    "https://jsonkeeper.com/b/KI8E", // Thunder Dragonroar
-    "https://jsonkeeper.com/b/5GG7", // Thunder Dragonmatrix
-    "https://jsonkeeper.com/b/5GG7", // Thunder Dragonmatrix
-    "https://jsonkeeper.com/b/5GG7", // Thunder Dragonmatrix
-    "https://jsonkeeper.com/b/4576", // Thunder Dragonlord
-    "https://jsonkeeper.com/b/4576", // Thunder Dragonlord
-    "https://jsonkeeper.com/b/4576", // Thunder Dragonlord
-    "https://jsonkeeper.com/b/8DP5", // Thunder Dragon Fusion
-    "https://jsonkeeper.com/b/8DP5", // Thunder Dragon Fusion
-    "https://jsonkeeper.com/b/8DP5", // Thunder Dragon Fusion
-    "https://jsonkeeper.com/b/LBO2", // Thunder Dragons' Hundred Thunders
-    "https://jsonkeeper.com/b/LBO2", // Thunder Dragons' Hundred Thunders
-    "https://jsonkeeper.com/b/LBO2", // Thunder Dragons' Hundred Thunders
-    "https://jsonkeeper.com/b/NU1G", // Thunder Dragon Discharge
-    "https://jsonkeeper.com/b/NU1G", // Thunder Dragon Discharge
-    "https://jsonkeeper.com/b/NU1G", // Thunder Dragon Discharge
+    "https://jsonkeeper.com/b/LQXT",
+    // "https://jsonkeeper.com/b/BLPT",
+    "https://jsonkeeper.com/b/MJ8Q",
+    // "https://jsonkeeper.com/b/7TRH"
+
+    
 ];
-  const nft = await Nft.deploy('Thunder Dragons', 'TD', "0xB5B970E3Cdd81D548F48c741573CCBEA8Ae4f2aF", uris);
+  const nft = await Nft.deploy('DDD Cars Collection', 'DDD', "0xB7e224427A3393f4B02D7098Fc5830895d564740", uris);
   await nft.deployed();
-  console.log('ERC721 deployed to:', nft.address);
+  console.log('Collection deployed to:', nft.address);
 }
 main()
   .then(() => process.exit(0))
