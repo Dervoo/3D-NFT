af415e065b1c7fddfaa16ef765690a069a781117
Dervoo <kojar1997@gmail.com>
2022-08-23 18:58:39 +0200

    format script and contract

--- scripts/deployCollection.js
+++ scripts/deployCollection.js
@@ -1,25 +1,27 @@
-const hre = require('hardhat');
-const fs = require('fs');
+const hre = require("hardhat");
+const fs = require("fs");
 /* 
   To deploy the smart contracts use
   npx hardhat run --network localhost scripts/deploy.js
 */
 async function main() {
-
-  const Nft = await hre.ethers.getContractFactory('Nft');
-    const uris = [
+  const Nft = await hre.ethers.getContractFactory("Nft");
+  const uris = [
     "https://jsonkeeper.com/b/LQXT",
     "https://jsonkeeper.com/b/MJ8Q",
-
-    
-];
-  const nft = await Nft.deploy('DDD Cars Collection', 'DDD', "0xB7e224427A3393f4B02D7098Fc5830895d564740", uris);
+  ];
+  const nft = await Nft.deploy(
+    "DDD Cars Collection",
+    "DDD",
+    "0xB7e224427A3393f4B02D7098Fc5830895d564740",
+    uris
+  );
   await nft.deployed();
-  console.log('Collection deployed to:', nft.address);
+  console.log("Collection deployed to:", nft.address);
 }
 main()
   .then(() => process.exit(0))
-  .catch(error => {
+  .catch((error) => {
     console.error(error);
     process.exit(1);
   });
